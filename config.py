7de2fa603fbb594257137d4a7ed50fda9cb401f6
Cjbot67658 <manassahu9044889@gmail.com>
2026-01-30 01:08:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 # Read from environment variables, with safe defaults for local dev
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
+API_ID = int(os.getenv("API_ID", "27857521"))
+API_HASH = os.getenv("API_HASH", "627b314d25c83e2c9a1a99db9ae0a3ef")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "123456:ABC-DEF...")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://username:password@cluster0.mongodb.net/storybot?retryWrites=true&w=majority")
-OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS", "111111111").split(",") if x.strip()]
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://alpmanas56_db_user:njoXL0eSLLVsE5rH@cluster0.fi3wrdi.mongodb.net/?appName=Cluster0")
+OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS", "7024087501").split(",") if x.strip()]
 DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID", "-1001234567890"))
 
