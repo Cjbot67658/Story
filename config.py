ca8c289c721c9af6b3e7a7e7c4988ffbe46201d2
Cjbot67658 <manassahu9044889@gmail.com>
2026-01-30 12:21:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ import os
 API_ID = int(os.getenv("API_ID", "27857521"))
 API_HASH = os.getenv("API_HASH", "627b314d25c83e2c9a1a99db9ae0a3ef")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8317617574:AAFQZD6lGCEtK2oIf_qNSDpe_zpqKYAugyQ")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://alpmanas56_db_user:njoXL0eSLLVsE5rH@cluster0.fi3wrdi.mongodb.net/?appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://alpmanas56_db_user:njoXL0eSLLVsE5rH@cluster0.fi3wrdi.mongodb.net/?appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 OWNER_IDS = [int(x) for x in os.getenv("OWNER_IDS", "7024087501").split(",") if x.strip()]
 DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID", "-1002342989997"))
 
